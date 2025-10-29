419de8419c461a85ae152880aa5a89965010ff11
YaqoubYousef <yaqoubyousef84@gmail.com>
2025-10-29 19:51:02 +0300

    fix: remove unwanted components

--- PROJECT_COMMENTS.md
+++ PROJECT_COMMENTS.md
@@ -1,9 +1,11 @@
-### Commit 
+### Commit
+
 `  ` Month/Day
 **Purpose:**  
 < >
 
 ### Commit
+
 `Add: enable the patient to choose an insurance company from the insurances table and access,update insurance company in useraccount`
 
 **Purpose:**  
@@ -12,35 +14,43 @@
 'Fix: Docker Files' Oct/25
 <frontend service wasn't working>
 
-### commit 
+### commit
+
 `Fix docker problems `
 < i made this commit to fix some docker problems . The problem was when i made any change in frontend i cannot see the result of that change.>
 
+### commit
 
-
-### commit 
 `feat(frontend): enable each clinic to manage its insurance companies add,delete,read `
 `feat(backend): enable each clinic to manage its insurance companies add,delete,read `
 < i made this commit to enable each clinic to add delete read insurance companies by handle the insurance controller , insuranceClinic,Clinic models modify the insurances table by adding new column to handle soft delete feature , then test the backend APIs using postman and make sure all works successfully , then make the frontend design and connect it with backend by axios library finally test the user interface and make sure the design is taking the MediCina theme >
 
+### Commit
 
-### Commit 
-` feat: enable each clinic to delete its associated doctors handle backend,frontend `
+`feat: enable each clinic to delete its associated doctors handle backend,frontend`
 **Purpose:**  
 < operate delete doctor from clinic also making it soft delete >
 
-### Commit 
-` cleaning `
+### Commit
+
+`cleaning`
 **Purpose:**  
 < remove the doctorclinic controller and but its functions inside the clinicdoctor controller becuase the confusion issue between the naming of these two controllers>
 
+### Commit
 
-### Commit 
-` feat: enable the patient to filter using the insurance company `
+`feat: enable the patient to filter using the insurance company`
 **Purpose:**  
-< enable each patient to filter by its insurance company so now before choose the clinic he can check if his prefered clinic use the same as his insurance company  >
+< enable each patient to filter by its insurance company so now before choose the clinic he can check if his prefered clinic use the same as his insurance company >
 
 ### Commit
+
 ` Add: Create and Update consultation_duration for doctors.` Oct/29
-**Purpose:** 
+**Purpose:**
 <Purpose is clear>
+
+### Commit
+
+` fix: remove unwanted components` Oct/29
+**Purpose:**
+<Caused a failure on server launch>
--- medicina-frontend/src/router/AppRouter.jsx
+++ medicina-frontend/src/router/AppRouter.jsx
@@ -1,175 +1,219 @@
-import React, { useEffect, Fragment } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect, Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import HomePage from '../pages/HomePage'
-import AboutPage from '../pages/AboutPage'
-import ContactPage from '../pages/ContactPage'
-import ClinicsPage from '../pages/ClinicsPage'
-import PatientRegisterPage from '../pages/RegisterPages/PatientRegisterPage'
-import PatientLoginPage from '../pages/LoginPages/PatientLoginPage'
-import AllClinicsPage from '../pages/AllClinicsPage'
-import DoctorProfilePage from '../pages/DoctorProfilePage'
-import AppointmentsSchedulePage from '../pages/AppointmentsSchedulePage'
-import ClinicRegisterPage from '../pages/RegisterPages/ClinicRegisterPage'
-import ClinicLoginPage from '../pages/LoginPages/ClinicLoginPage'
-import DoctorLoginPage from '../pages/LoginPages/DoctorLoginPage'
-import DoctorRegisterPage from '../pages/RegisterPages/DoctorRegisterPage'
-import LabLoginPage from '../pages/LoginPages/LabLoginPage'
-import LabRegisterPage from '../pages/RegisterPages/LabRegisterPage'
-import ChangePasswordPage from '../pages/AccountPages/ChangePasswordPage'
-import DoctorBioPage from '../pages/AccountPages/DoctorBioPage'
-import SideBar from '../components/SideBar'
-import ProtectedRoute from '../components/ProtectedRoute'
-import Unauthorized from '../pages/Unauthorized'
-import NotFound from '../pages/NotFound'
-import RoleAccountRoute from '../components/RoleAccountRoute'
-import ManageDoctorsPage from '../pages/ManageDoctorsPage'
-import DoctorManagementPage from '../pages/DoctorManagementPage'
-import UpComingAppointmentPage from '../pages/UpComingAppointmentPage'
-import PastAppointmentPage from '../pages/PastAppointmentPage'
-import ClinicAppointmentsPage from '../pages/ClinicAppointmentsPage'
-import ClinicInsurancePage from '../pages/ClinicInsurancePage'
-import PatientDataPage from '../pages/PatientDataPage'
-import EmailVerificationSuccess from '../components/EmailVerificationSuccess'
-import ForgotPassword from '../components/ForgotPassword'
-import ResetPassword from '../components/ResetPassword'
-import MedicalRecordEntryPage from '../pages/MedicalRecordEntryPage'
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import ClinicsPage from "../pages/ClinicsPage";
+import PatientRegisterPage from "../pages/RegisterPages/PatientRegisterPage";
+import PatientLoginPage from "../pages/LoginPages/PatientLoginPage";
+import AllClinicsPage from "../pages/AllClinicsPage";
+import DoctorProfilePage from "../pages/DoctorProfilePage";
+import AppointmentsSchedulePage from "../pages/AppointmentsSchedulePage";
+import ClinicRegisterPage from "../pages/RegisterPages/ClinicRegisterPage";
+import ClinicLoginPage from "../pages/LoginPages/ClinicLoginPage";
+import DoctorLoginPage from "../pages/LoginPages/DoctorLoginPage";
+import DoctorRegisterPage from "../pages/RegisterPages/DoctorRegisterPage";
+import LabLoginPage from "../pages/LoginPages/LabLoginPage";
+import LabRegisterPage from "../pages/RegisterPages/LabRegisterPage";
+import ChangePasswordPage from "../pages/AccountPages/ChangePasswordPage";
+import DoctorBioPage from "../pages/AccountPages/DoctorBioPage";
+import SideBar from "../components/SideBar";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Unauthorized from "../pages/Unauthorized";
+import NotFound from "../pages/NotFound";
+import RoleAccountRoute from "../components/RoleAccountRoute";
+import ManageDoctorsPage from "../pages/ManageDoctorsPage";
+import DoctorManagementPage from "../pages/DoctorManagementPage";
+import UpComingAppointmentPage from "../pages/UpComingAppointmentPage";
+import PastAppointmentPage from "../pages/PastAppointmentPage";
+import ClinicAppointmentsPage from "../pages/ClinicAppointmentsPage";
+import ClinicInsurancePage from "../pages/ClinicInsurancePage";
+import PatientDataPage from "../pages/PatientDataPage";
+import EmailVerificationSuccess from "../components/EmailVerificationSuccess";
+import ForgotPassword from "../components/ForgotPassword";
+import ResetPassword from "../components/ResetPassword";
 
 const AppRouter = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
+	useEffect(() => {
+		AOS.init({
+			duration: 1000,
+			once: true,
+		});
+	}, []);
 
-  return (
-    <Fragment>
-      
-      <Routes>
-        <Route path="/" element={<HomePage  />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path='/contact' element={<ContactPage/>}/>
+	return (
+		<Fragment>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/contact" element={<ContactPage />} />
 
-        {/* Clinics: allow guests + patients */}
-          <Route
-            path="/clinics"
-            element={
-              <ProtectedRoute allowedRoles={['patient', 'guest']}>
-                <ClinicsPage />
-              </ProtectedRoute>
-            }
-          />
-        {/* Routes For Login And Register */}
-        {/* for patient */}
-        <Route path='/login/patient' element={<PatientLoginPage />} />
-        <Route path='/register/patient' element={ <PatientRegisterPage/>}/>
-        {/* end patient */}
+				{/* Clinics: allow guests + patients */}
+				<Route
+					path="/clinics"
+					element={
+						<ProtectedRoute allowedRoles={["patient", "guest"]}>
+							<ClinicsPage />
+						</ProtectedRoute>
+					}
+				/>
+				{/* Routes For Login And Register */}
+				{/* for patient */}
+				<Route path="/login/patient" element={<PatientLoginPage />} />
+				<Route
+					path="/register/patient"
+					element={<PatientRegisterPage />}
+				/>
+				{/* end patient */}
 
-        {/* for clinic */}
-        <Route path='/login/clinic' element={ <ClinicLoginPage/>}/>
-        <Route path='/register/clinic' element={ <ClinicRegisterPage/>}/>
-        {/* end clinic */}
+				{/* for clinic */}
+				<Route path="/login/clinic" element={<ClinicLoginPage />} />
+				<Route
+					path="/register/clinic"
+					element={<ClinicRegisterPage />}
+				/>
+				{/* end clinic */}
 
-        {/* for doctor */}
-        <Route path='/login/doctor' element={ <DoctorLoginPage/>}/>
-        <Route path='/register/doctor' element={ <DoctorRegisterPage/>}/>
-        {/* end doctor */}
+				{/* for doctor */}
+				<Route path="/login/doctor" element={<DoctorLoginPage />} />
+				<Route
+					path="/register/doctor"
+					element={<DoctorRegisterPage />}
+				/>
+				{/* end doctor */}
 
-        {/* for lab */}
-        <Route path='/login/lab' element={ <LabLoginPage/>}/>
-        <Route path='/register/lab' element={ <LabRegisterPage/>}/>
-        {/* end lab */}
-        {/* End For Login And Register */}
+				{/* for lab */}
+				<Route path="/login/lab" element={<LabLoginPage />} />
+				<Route path="/register/lab" element={<LabRegisterPage />} />
+				{/* end lab */}
+				{/* End For Login And Register */}
 
-         <Route
-            path="/clinics/:directory"
-            element={
-              <ProtectedRoute allowedRoles={['patient', 'guest']}>
-                <AllClinicsPage />
-              </ProtectedRoute>
-            }
-          />
-        <Route path='/doctor/profile/:id'  element={ <DoctorProfilePage/>}/>
-        <Route path='/doctor/appointment/schedule/:id'  element={ <AppointmentsSchedulePage/>}/>
-        <Route path='/doctor/medical-records/new' element={
-          <ProtectedRoute allowedRoles={['doctor']}>
-            <MedicalRecordEntryPage />
-          </ProtectedRoute>
-        }/>
-        
-        {/* Upcoming Appointments: only for patients */}
-        <Route
-          path="/patient/upcoming-appointments"
-          element={
-            <ProtectedRoute allowedRoles={['patient']}>
-              <UpComingAppointmentPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/patient/past-appointments"
-          element={
-            <ProtectedRoute allowedRoles={['patient']}>
-              <PastAppointmentPage />
-            </ProtectedRoute>
-          }
-        />
-     
-        <Route path='/sidebar' element={<SideBar/>}/>
+				<Route
+					path="/clinics/:directory"
+					element={
+						<ProtectedRoute allowedRoles={["patient", "guest"]}>
+							<AllClinicsPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/doctor/profile/:id"
+					element={<DoctorProfilePage />}
+				/>
+				<Route
+					path="/doctor/appointment/schedule/:id"
+					element={<AppointmentsSchedulePage />}
+				/>
 
-        <Route path="/email-verified" element={<EmailVerificationSuccess />} />
-        <Route path="/email-verification-failed" element={<EmailVerificationSuccess />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/:role/account" element={<RoleAccountRoute />} />
-        <Route path="/:role/change-password" element={
-          <ProtectedRoute allowedRoles={['patient', 'doctor', 'clinic', 'lab']}>
-            <ChangePasswordPage />
-          </ProtectedRoute>
-        } />
-        <Route path="/doctor/bio" element={
-          <ProtectedRoute allowedRoles={['doctor']}>
-            <DoctorBioPage />
-          </ProtectedRoute>
-        } />
-        <Route path="/unauthorized" element={<Unauthorized />} />
-        <Route path="/404" element={<NotFound />} />
+				{/* Upcoming Appointments: only for patients */}
+				<Route
+					path="/patient/upcoming-appointments"
+					element={
+						<ProtectedRoute allowedRoles={["patient"]}>
+							<UpComingAppointmentPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/patient/past-appointments"
+					element={
+						<ProtectedRoute allowedRoles={["patient"]}>
+							<PastAppointmentPage />
+						</ProtectedRoute>
+					}
+				/>
 
-        <Route path="/manage/doctors" element={
-          <ProtectedRoute allowedRoles={['clinic']}>
-          <ManageDoctorsPage/>
-          </ProtectedRoute>
-          }/>
-        <Route path="/manage/doctor/:doctorId/:clinicId" element={
-          <ProtectedRoute allowedRoles={['clinic']}>
-          <DoctorManagementPage/>
-          </ProtectedRoute>
-          }/>
-        <Route path="/clinic/appointments" element={
-          <ProtectedRoute allowedRoles={['clinic']}>
-          <ClinicAppointmentsPage/>
-          </ProtectedRoute>
-          }/>
-        <Route path="/clinic/insurances" element={
-          <ProtectedRoute allowedRoles={['clinic']}>
-          <ClinicInsurancePage/>
-          </ProtectedRoute>
-          }/>
-        <Route path="/patients/by-user-id/:user_id" element={
-          <ProtectedRoute allowedRoles={['clinic']}>
-          <PatientDataPage/>
-          </ProtectedRoute>
-          }/>
-        
-        
-        {/* Catch-all route for 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Fragment>
-  );
+				<Route path="/sidebar" element={<SideBar />} />
+
+				<Route
+					path="/email-verified"
+					element={<EmailVerificationSuccess />}
+				/>
+				<Route
+					path="/email-verification-failed"
+					element={<EmailVerificationSuccess />}
+				/>
+				<Route path="/forgot-password" element={<ForgotPassword />} />
+				<Route
+					path="/reset-password/:token"
+					element={<ResetPassword />}
+				/>
+				<Route path="/:role/account" element={<RoleAccountRoute />} />
+				<Route
+					path="/:role/change-password"
+					element={
+						<ProtectedRoute
+							allowedRoles={[
+								"patient",
+								"doctor",
+								"clinic",
+								"lab",
+							]}
+						>
+							<ChangePasswordPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/doctor/bio"
+					element={
+						<ProtectedRoute allowedRoles={["doctor"]}>
+							<DoctorBioPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="/unauthorized" element={<Unauthorized />} />
+				<Route path="/404" element={<NotFound />} />
+
+				<Route
+					path="/manage/doctors"
+					element={
+						<ProtectedRoute allowedRoles={["clinic"]}>
+							<ManageDoctorsPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/manage/doctor/:doctorId/:clinicId"
+					element={
+						<ProtectedRoute allowedRoles={["clinic"]}>
+							<DoctorManagementPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/clinic/appointments"
+					element={
+						<ProtectedRoute allowedRoles={["clinic"]}>
+							<ClinicAppointmentsPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/clinic/insurances"
+					element={
+						<ProtectedRoute allowedRoles={["clinic"]}>
+							<ClinicInsurancePage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/patients/by-user-id/:user_id"
+					element={
+						<ProtectedRoute allowedRoles={["clinic"]}>
+							<PatientDataPage />
+						</ProtectedRoute>
+					}
+				/>
+
+				{/* Catch-all route for 404 */}
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Fragment>
+	);
 };
 
-export default AppRouter
+export default AppRouter;
